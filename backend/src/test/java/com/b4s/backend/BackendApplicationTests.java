d85f46b7bb72a84e1138c27b4b7f80dce47dbaef
Filipe Moura <filipeasm18@gmail.com>
2022-08-13 22:28:30 -0300

    Initial commit

---
+++ backend/src/test/java/com/b4s/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b4s.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
