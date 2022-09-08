1e6fe75302706d762ce4b3b1c26519abfdfe0736
Filipe <filipeasm18@gmail.com>
2022-09-07 22:05:42 -0300

    Feat: Person model impl

---
+++ backend/src/main/java/com/b4s/backend/domain/Person.java
@@ -0,0 +1,31 @@
+package com.b4s.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "pessoa")
+public class Person {
+
+    @Id
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "endereco")
+    private String address;
+
+}
