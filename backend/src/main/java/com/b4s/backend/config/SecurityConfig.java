15619080913f63780abfa2f604e83b13fbe1bdc1
Filipe <filipeasm18@gmail.com>
2022-09-07 22:06:24 -0300

    Feat: Initial HTTP Configs

---
+++ backend/src/main/java/com/b4s/backend/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.b4s.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/api/v1/person/**").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/v1/person").permitAll();
+    }
+}
