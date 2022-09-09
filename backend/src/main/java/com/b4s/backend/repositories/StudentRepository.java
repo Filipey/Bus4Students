1f91faae7a2f207a054818144a4f14576527db7e
Filipe <filipeasm18@gmail.com>
2022-09-09 00:16:42 -0300

    Feat: Created InsertStudent Query

--- backend/src/main/java/com/b4s/backend/repositories/StudentRepository.java
+++ backend/src/main/java/com/b4s/backend/repositories/StudentRepository.java
@@ -2,6 +2,7 @@ package com.b4s.backend.repositories;
 
 import com.b4s.backend.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,9 @@ public interface StudentRepository extends JpaRepository<Student, String> {
     @Query(nativeQuery = true, value = "SELECT * FROM pessoa p INNER JOIN estudante e on p.cpf = :cpf AND e.fk_pessoa_cpf = :cpf")
     Optional<Student> getStudentByCpf(@Param("cpf") String cpf);
 
+    @Modifying
+    @Query(nativeQuery = true, value =
+            "INSERT INTO pessoa(cpf, nome, endereco) VALUES (:#{#student.cpf}, :#{#student.name}, :#{#student.address});" +
+            "INSERT INTO estudante(comprovante_de_matricula_, fk_pessoa_cpf) VALUES(:#{#student.enrollment}, :#{#student.cpf}) ")
+    void create(@Param("student") Student student);
 }
