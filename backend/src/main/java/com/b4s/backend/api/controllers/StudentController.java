f616dfdc194d522026f234897af5cb8b0b71cf6f
Filipe <filipeasm18@gmail.com>
2022-09-08 22:58:35 -0300

    Update: Updated Controller with Swagger config

--- backend/src/main/java/com/b4s/backend/api/controllers/StudentController.java
+++ backend/src/main/java/com/b4s/backend/api/controllers/StudentController.java
@@ -2,11 +2,15 @@ package com.b4s.backend.api.controllers;
 
 import com.b4s.backend.domain.Student;
 import com.b4s.backend.services.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/student")
+@Api("Students API")
 public class StudentController {
 
     private final StudentService studentService;
@@ -16,6 +20,10 @@ public class StudentController {
     }
 
     @GetMapping("{cpf}")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Student found"),
+            @ApiResponse(code = 404, message = "Student not found")
+    })
     @ResponseStatus(HttpStatus.OK)
     public Student getStudentByCpf(@PathVariable String cpf) {
         return studentService.getStudentByCpf(cpf);
