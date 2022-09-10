9f61baecbc1c24887beb29e02de754627a3322c3
Filipe <filipeasm18@gmail.com>
2022-09-10 16:21:03 -0300

    Feat: Created School entity

---
+++ backend/src/main/java/com/b4s/backend/domain/School.java
@@ -0,0 +1,31 @@
+package com.b4s.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "instituicao_de_ensino")
+public class School {
+
+    @Id
+    private String campus;
+
+    @Column(name = "nome_")
+    private String name;
+
+    @Column(name = "localizacao")
+    private String location;
+
+    @Column(name = "periodo_letivo")
+    private boolean isActive;
+
+    @ManyToMany(mappedBy = "schools", cascade = CascadeType.ALL)
+    private List<Student> students;
+}
