97984ef9ac9ee98252396190343c5fec64265f94
Filipe <filipeasm18@gmail.com>
2022-09-10 16:20:36 -0300

    Fix: Fixed Person Inheritance table generation

--- backend/src/main/java/com/b4s/backend/domain/Person.java
+++ backend/src/main/java/com/b4s/backend/domain/Person.java
@@ -1,20 +1,17 @@
 package com.b4s.backend.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "pessoa")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Person {
 
     @Id
