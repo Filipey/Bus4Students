37b2143ce421dde1cf63be44f695a9bef279a407
Filipe <filipeasm18@gmail.com>
2022-09-11 13:24:58 -0300

    Refactor: Better Lombok import

--- backend/src/main/java/com/b4s/backend/domain/Student.java
+++ backend/src/main/java/com/b4s/backend/domain/Student.java
@@ -1,8 +1,7 @@
 package com.b4s.backend.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
