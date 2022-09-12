945b93ca372b8dfa91b1ed0421130299b1514557
Filipe <filipeasm18@gmail.com>
2022-09-11 23:03:36 -0300

    Feat: Created TicketService

---
+++ backend/src/main/java/com/b4s/backend/services/TicketService.java
@@ -0,0 +1,18 @@
+package com.b4s.backend.services;
+
+import com.b4s.backend.domain.Ticket;
+
+import java.util.List;
+
+public interface TicketService {
+
+    Ticket getById(int id);
+
+    void insert(Ticket ticket);
+
+    void update(Ticket ticket, int id);
+
+    void delete(int id);
+
+    List<Ticket> getAllDisponibleTickets();
+}
