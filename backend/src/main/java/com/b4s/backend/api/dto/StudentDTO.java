aeed8be6ec9aca9ce18e288e7e77937ae72ba766
Filipe <filipeasm18@gmail.com>
2022-09-20 00:25:17 -0300

    Refactor: Removed Mapping from DTO

--- backend/src/main/java/com/b4s/backend/api/dto/StudentDTO.java
+++ backend/src/main/java/com/b4s/backend/api/dto/StudentDTO.java
@@ -1,12 +1,8 @@
 package com.b4s.backend.api.dto;
 
-import com.b4s.backend.domain.Bus;
-import com.b4s.backend.domain.School;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -16,7 +12,5 @@ public class StudentDTO {
     private String name;
     private String cpf;
     private String enrollment;
-    private List<School> schools;
-    private List<Bus> buses;
 
 }
