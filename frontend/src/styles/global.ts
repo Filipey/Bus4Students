de5c874bc7de5673b3d5cc55d5a202006ace1c77
Diogo Leite <diogoleite87@gmail.com>
2022-09-21 10:19:32 -0300

    feat: environment configuration

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+    * {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+`
