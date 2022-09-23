678e0ec502abbc2a3d64a9e58d70d179e3c15259
Filipe <filipeasm18@gmail.com>
2022-09-23 14:38:16 -0300

    Fix: converted ListItem to component

--- frontend/src/components/ContainerAdmin/index.tsx
+++ frontend/src/components/ContainerAdmin/index.tsx
@@ -1,18 +1,14 @@
-import * as S from './style';
-import HeaderAdmin from '../HeaderAdmin';
-import { Container } from '@material-ui/core';
-import Box from '@material-ui/core';
+import { Container } from '@material-ui/core'
+import HeaderAdmin from '../HeaderAdmin'
+import * as S from './style'
 
 export function ContainerAdmin() {
-    return (
-        <S.Container>
-
-            <HeaderAdmin page='Administrador: X' />
-            <Container>
-                <h1>sdsd</h1>
-            </Container>
-
-        </S.Container>
-
-    );
+  return (
+    <S.Container>
+      <HeaderAdmin page="Administrador: X" />
+      <Container>
+        <h1>sdsd</h1>
+      </Container>
+    </S.Container>
+  )
 }
