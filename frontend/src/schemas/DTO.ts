16a956c899925993e5062c3b857bdbdaf9a1f810
Filipe <filipeasm18@gmail.com>
2022-09-24 18:40:50 -0300

    Style: reformatted file

--- frontend/src/schemas/DTO.ts
+++ frontend/src/schemas/DTO.ts
@@ -1,20 +1,33 @@
 export interface StudentPassDTO {
-    // expirationDate: string($date-time),
-    expirationDate: string,
-    schoolName: string,
-    studentCpf: string,
+  // expirationDate: string($date-time),
+  expirationDate: string
+  schoolName: string
+  studentCpf: string
 }
 
 export interface StudentDTO {
-    cpf: string;
-    name: string;
-    address: string;
-    enrollment: string;
+  cpf: string
+  name: string
+  address: string
+  enrollment: string
 }
 
 export interface HallBusDTO {
-    driver: string,
-    // passengersLimit:	integer($int32),
-    passengersLimit: number;
+  driver: string
+  // passengersLimit:	integer($int32),
+  passengersLimit: number
+}
 
+export interface PersonResponseDTO {
+  cpf: string
+  name: string
+  address: string
+  enrollment: string
+  idAdmin: number
+  role: string
+}
+
+export interface UserDTO {
+  cpf: string
+  password: string
 }
--- frontend/src/schemas/Models.ts
+++ frontend/src/schemas/Models.ts
@@ -1,59 +1,68 @@
 export interface Bus {
   departureTime: string
   plate: string
-
 }
 
 export interface EsconBus {
-  departureTime: string,
+  departureTime: string
   // line: +integer($int32),
-  line: number;
+  line: number
   plate: string
 }
 
 export interface HallBus {
-  departureTime: string,
-  driver: string,
+  departureTime: string
+  driver: string
   // passengersLimit: integer($int32),
-  passengersLimit: number,
+  passengersLimit: number
   plate: string
 }
 
 export interface School {
-  active: boolean,
-  campus: string,
-  location: string,
-  name: string,
+  active: boolean
+  campus: string
+  location: string
+  name: string
   students: Student[]
 }
 
 export interface Student {
-  cpf: string;
-  name: string;
-  address: string;
-  enrollment: string;
-  schools: School[];
-  buses: Bus[];
+  cpf: string
+  name: string
+  address: string
+  enrollment: string
+  schools: School[]
+  buses: Bus[]
 }
 
 export interface StudentPass {
   // expirationDate:	string($date),
   // id:	integer($int64),
-  expirationDate: string,
-  id: number,
-  owner: Student,
+  expirationDate: string
+  id: number
+  owner: Student
   schoolName: string
 }
 
 export interface Ticket {
   // expirationDate:	string($date),
   // id:	integer($int32),
-  expirationDate: string,
-  id: number,
-  sink: string,
-  source: string,
+  expirationDate: string
+  id: number
+  sink: string
+  source: string
   value: number
 }
 
+export interface User {
+  cpf: string
+  password: string
+  isAdmin: boolean
+}
 
-
+export enum UserRoles {
+  STUDENT,
+  ADMIN,
+  SYS_ADMIN,
+  NONE
+}
