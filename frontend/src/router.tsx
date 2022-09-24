ce6188881aa9f7345a154bbc69e94e572143eb54
Filipe <filipeasm18@gmail.com>
2022-09-24 18:39:45 -0300

    Fix: Provided Theme on routes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@mui/material/styles'
 import { Route, Routes } from 'react-router-dom'
 import { UserContextProvider } from './hooks/userContext'
 
@@ -6,19 +7,22 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import User from './pages/User'
+import { theme } from './utils/theme'
 
 export function Router() {
   return (
     <>
-      <UserContextProvider>
-        <Routes>
-          <Route element={<Home />} path="/" />
-          <Route element={<Login />} path="/login" />
-          <Route element={<Register />} path="/register" />
-          <Route element={<User />} path="/user" />
-          <Route element={<Admin />} path="/admin" />
-        </Routes>
-      </UserContextProvider>
+      <ThemeProvider theme={theme}>
+        <UserContextProvider>
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Login />} path="/login" />
+            <Route element={<Register />} path="/register" />
+            <Route element={<User />} path="/user" />
+            <Route element={<Admin />} path="/admin" />
+          </Routes>
+        </UserContextProvider>
+      </ThemeProvider>
     </>
   )
 }
