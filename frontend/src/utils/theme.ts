14d2f59b6cb5d3e81460f50b0c4845b24306122d
Filipe <filipeasm18@gmail.com>
2022-09-24 18:41:32 -0300

    Refactor: Refactored global styles

--- frontend/src/utils/theme.ts
+++ frontend/src/utils/theme.ts
@@ -1,41 +1,75 @@
-import { createTheme } from '@material-ui/core/styles';
-
 export const THEME = {
-    COLORS: {
-      BACKGROUND_900: '',
-      BACKGROUND_800: '',
-  
-      TEXT: '#FFFFFF',
-  
-      CAPTION_500: '#71717A',
-      CAPTION_400: '#A1A1AA',
-      CAPTION_300: '#D4D4D8',
-  
-      SHAPE: '#2A2634',
-  
-      COLOR1: '#7686ea',
-      COLOR2: '#6176d7',
-      COLOR3: '#4b66c5',
-      COLOR4: '#3655b2',
-      COLOR5: '#20459f', 
+  COLORS: {
+    BACKGROUND_900: '',
+    BACKGROUND_800: '',
 
-      SUCCESS: '#34D399',
-      ALERT: '#F87171',
-  
-      FOOTER: '',
-      OVERLAY: '',
+    TEXT: '#FFFFFF',
+
+    CAPTION_500: '#71717A',
+    CAPTION_400: '#A1A1AA',
+    CAPTION_300: '#D4D4D8',
+
+    SHAPE: '#2A2634',
+
+    COLOR1: '#7686ea',
+    COLOR2: '#6176d7',
+    COLOR3: '#4b66c5',
+    COLOR4: '#3655b2',
+    COLOR5: '#20459f',
+
+    SUCCESS: '#34D399',
+    ALERT: '#F87171',
+
+    FOOTER: '',
+    OVERLAY: ''
+  },
+
+  FONT_FAMILY: {
+    REGULAR: '',
+    SEMI_BOLD: '',
+    BOLD: '',
+    BLACK: ''
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 24
+  }
+}
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#7686ea',
+      main: '#4b66c5',
+      dark: '#20459f'
     },
-  
-    FONT_FAMILY: {
-      REGULAR: '',
-      SEMI_BOLD: '',
-      BOLD: '',
-      BLACK: ''
+    secondary: {
+      light: 'rgb(112, 112, 112)',
+      main: '#ffff',
+      dark: 'rgb(53, 53, 53)'
     },
-  
-    FONT_SIZE: {
-      SM: 14,
-      MD: 16,
-      LG: 24
+    error: {
+      light: '#ef5350',
+      main: '#d32f2f',
+      dark: '#c62828'
+    },
+    warning: {
+      light: '#ff9800',
+      main: '#ed6c02',
+      dark: '#e65100'
+    },
+    info: {
+      light: '#03a9f4',
+      main: '#0288d1',
+      dark: '#01579b'
+    },
+    success: {
+      light: '#4caf50',
+      main: '#2e7d32',
+      dark: '#1b5e20'
     }
-  };
+  }
+})
