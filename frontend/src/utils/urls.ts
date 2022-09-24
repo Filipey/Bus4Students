17d8655054cbcdb64634047bb44de59d5db32a1a
Filipe <filipeasm18@gmail.com>
2022-09-24 18:41:42 -0300

    Feat: initial base urls for users

---
+++ frontend/src/utils/urls.ts
@@ -0,0 +1,16 @@
+export const studentUrls = [
+  '/user',
+  '/user',
+  '/user',
+  '/user',
+  '/user',
+  '/user'
+]
+export const adminUrls = [
+  '/admin',
+  '/admin',
+  '/admin',
+  '/admin',
+  '/admin',
+  '/admin'
+]
