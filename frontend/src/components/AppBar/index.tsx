730f13953443adfd9176a6bb48cedee4c8dc1047
Filipe <filipeasm18@gmail.com>
2022-09-24 18:27:17 -0300

    Feat: AppBar Component

---
+++ frontend/src/components/AppBar/index.tsx
@@ -0,0 +1,84 @@
+import { Person } from '@material-ui/icons'
+import { Logout } from '@mui/icons-material'
+import Menu from '@mui/icons-material/Menu'
+import { Typography } from '@mui/material'
+import MuiAppBar from '@mui/material/AppBar'
+import IconButton from '@mui/material/IconButton'
+import { styled } from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import UserContext from '../../hooks/userContext'
+
+interface AppBarProps {
+  open?: boolean
+  toggleDrawer(): void
+}
+
+interface AppBarSetup {
+  open?: boolean
+}
+
+const AppBarSetup = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open'
+})<AppBarSetup>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: 240,
+    width: `calc(100% - ${240}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
+
+export function AppBar({ open, toggleDrawer }: AppBarProps) {
+  const navigate = useNavigate()
+  const { user, setUser } = useContext(UserContext)
+
+  const handleLogout = () => {
+    setUser({
+      cpf: '',
+      name: '',
+      address: '',
+      role: ''
+    })
+    navigate('/')
+  }
+
+  return (
+    <AppBarSetup position="absolute" open={open}>
+      <Toolbar sx={{ pr: '24px' }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open-drawer"
+          onClick={toggleDrawer}
+          sx={{ marginRight: '36px', ...(open && { display: 'none' }) }}
+        >
+          <Menu />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          {'Bem vindo, ' + user.name}
+        </Typography>
+        <IconButton>
+          <Person />
+        </IconButton>
+        <IconButton onClick={handleLogout}>
+          <Logout />
+        </IconButton>
+      </Toolbar>
+    </AppBarSetup>
+  )
+}
