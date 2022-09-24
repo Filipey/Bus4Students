da61dd4392e68a6d5289b536e602e47bfa861659
Filipe <filipeasm18@gmail.com>
2022-09-24 18:43:45 -0300

    Feat: Created UserContext

---
+++ frontend/src/hooks/userContext.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, ReactNode, useState } from 'react'
+
+type User = {
+  cpf: string
+  name: string
+  address: string
+  role: string
+}
+
+type UserContextProps = {
+  user: User
+  setUser: React.Dispatch<React.SetStateAction<User>>
+}
+
+interface UserContextProviderProps {
+  children?: ReactNode
+}
+
+const DEFAULT_VALUE = {
+  user: {
+    cpf: '',
+    name: '',
+    address: '',
+    role: ''
+  },
+  setUser: () => {}
+}
+
+const UserContext = createContext<UserContextProps>(DEFAULT_VALUE)
+
+function UserContextProvider({ children }: UserContextProviderProps) {
+  const [user, setUser] = useState(DEFAULT_VALUE.user)
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export { UserContextProvider }
+export default UserContext
