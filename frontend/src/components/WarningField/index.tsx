e4ec060430dbc39ee423526697ff1243431a5b84
Filipe <filipeasm18@gmail.com>
2022-09-24 21:21:47 -0300

    Feat: Created WarningField

---
+++ frontend/src/components/WarningField/index.tsx
@@ -0,0 +1,18 @@
+import { Alert, AlertTitle, Box } from '@mui/material'
+
+interface WarningFieldProps {
+  message: string
+  severity: 'success' | 'warning' | 'error' | 'info'
+  title?: string
+}
+
+export function WarningField({ message, severity, title }: WarningFieldProps) {
+  return (
+    <Box display="flex" sx={{ pt: '1vh', pb: '1vh', width: '100%' }}>
+      <Alert sx={{ width: '100%' }} severity={severity}>
+        {title && <AlertTitle>{title}</AlertTitle>}
+        {message}
+      </Alert>
+    </Box>
+  )
+}
