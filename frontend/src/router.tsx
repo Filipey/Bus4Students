8854c49cac0970f25ec7404329bad52170e7eccf
Filipe <filipeasm18@gmail.com>
2022-09-25 16:27:59 -0300

    Feat: Add Bus routes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import { UserContextProvider } from './hooks/userContext'
 
 import Admin from './pages/Admin'
+import { AdminBus } from './pages/Admin/Bus'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import User from './pages/User'
+import { UserAllBus, UserMyBus } from './pages/User/Bus'
 import { theme } from './utils/theme'
 
 export function Router() {
@@ -19,7 +21,10 @@ export function Router() {
             <Route element={<Login />} path="/login" />
             <Route element={<Register />} path="/register" />
             <Route element={<User />} path="/user" />
+            <Route element={<UserAllBus />} path="/user/bus/all" />
+            <Route element={<UserMyBus />} path="user/bus/my" />
             <Route element={<Admin />} path="/admin" />
+            <Route element={<AdminBus />} path="/admin/bus" />
           </Routes>
         </UserContextProvider>
       </ThemeProvider>
