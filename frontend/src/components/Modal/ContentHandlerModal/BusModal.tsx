76b01d6bcb207863b450ab98da46054de42f7e11
Filipe <filipeasm18@gmail.com>
2022-09-26 23:56:00 -0300

    Feat: Initial ActionHandling

--- frontend/src/components/Modal/ContentHandlerModal/BusModal.tsx
+++ frontend/src/components/Modal/ContentHandlerModal/BusModal.tsx
@@ -15,11 +15,12 @@ import {
   Grid,
   IconButton
 } from '@mui/material'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import UserContext from '../../../hooks/userContext'
 import { EsconBus, HallBus } from '../../../schemas'
-import { isHallBus } from '../../../utils/getType'
+import { isEsconBus, isHallBus } from '../../../utils/getType'
 import { InfoTextField } from '../../InfoTextField'
+import { WarningField } from '../../WarningField'
 
 interface BusContentProps {
   bus: EsconBus | HallBus
@@ -37,8 +38,16 @@ export function BusContentModal({
   index
 }: BusContentProps) {
   const { user } = useContext(UserContext)
-
-  const isUserAdmin = user.role === 'ADMIN'
+  const [plate, setPlate] = useState(bus.plate)
+  const [departureTime, setDepartureTime] = useState(bus.departureTime)
+  const [driver, setDriver] = useState(isHallBus(bus) ? bus.driver : '')
+  const [passengersLimit, setPassengersLimit] = useState(
+    isHallBus(bus) ? bus.passengersLimit : 0
+  )
+  const [line, setLine] = useState(isHallBus(bus) ? 0 : bus.line)
+  const [permission, setPermission] = useState(userPermission())
+  const [changes, setChanges] = useState<string[]>([])
+  const [changesWarning, setChangesWarning] = useState(false)
 
   function userPermission() {
     if (mode === 'view') {
@@ -52,7 +61,52 @@ export function BusContentModal({
     return true
   }
 
+  function hasChanged() {
+    if (plate !== bus.plate) {
+      changes.find(c => c === 'Placa')
+        ? setChanges(changes)
+        : setChanges([...changes, 'Placa'])
+    }
+    if (departureTime !== bus.departureTime) {
+      changes.find(c => c === 'Horário de Saída')
+        ? setChanges(changes)
+        : setChanges([...changes, 'Horário de Saída'])
+    }
+    if (isHallBus(bus)) {
+      if (driver !== bus.driver) {
+        changes.find(c => c === 'Motorista')
+          ? setChanges(changes)
+          : setChanges([...changes, 'Motorista'])
+      }
+      if (passengersLimit !== passengersLimit) {
+        changes.find(c => c === 'Limite de Passageiros')
+          ? setChanges(changes)
+          : setChanges([...changes, 'Limite de Passageiros'])
+      }
+    }
+    if (isEsconBus(bus)) {
+      if (line !== bus.line) {
+        changes.find(c => c === 'Linha')
+          ? setChanges(changes)
+          : setChanges([...changes, 'Linha'])
+      }
+    }
+  }
+
+  useEffect(() => {
+    hasChanged()
+    console.log(changes)
+  }, [plate, departureTime, driver, passengersLimit, line])
+
   function handleCloseModal() {
+    setPermission(true)
+    setPlate(bus.plate)
+    setDepartureTime(bus.departureTime)
+    setDriver(isHallBus(bus) ? bus.driver : '')
+    setPassengersLimit(isHallBus(bus) ? bus.passengersLimit : 0)
+    setLine(isHallBus(bus) ? 0 : bus.line)
+    setChanges([])
+    setChangesWarning(false)
     setState(state.map((i, pos) => (pos === index ? false : i)))
   }
 
@@ -80,39 +134,43 @@ export function BusContentModal({
             <InfoTextField
               label="Placa"
               defaultValue={bus.plate}
-              disabled={userPermission()}
+              disabled={permission}
               fullWidth={true}
               icon={<DirectionsBus />}
+              onChange={e => setPlate(e.target.value)}
             />
             <InfoTextField
               label="Horário de Saída"
               defaultValue={bus.departureTime}
-              disabled={userPermission()}
+              disabled={permission}
               fullWidth={true}
               icon={<Schedule />}
+              onChange={e => setDepartureTime(e.target.value)}
             />
             {isHallBus(bus) ? (
               <>
                 <InfoTextField
                   label="Fornecedor"
                   defaultValue="Prefeitura"
-                  disabled={userPermission()}
+                  disabled={true}
                   fullWidth={true}
                   icon={<AccountBalance />}
                 />
                 <InfoTextField
                   label="Motorista"
                   defaultValue={bus.driver}
-                  disabled={userPermission()}
+                  disabled={permission}
                   fullWidth={true}
                   icon={<AccountBox />}
+                  onChange={e => setDriver(e.target.value)}
                 />
                 <InfoTextField
                   label="Limite de Passageiros"
                   defaultValue={bus.passengersLimit.toString()}
-                  disabled={userPermission()}
+                  disabled={permission}
                   fullWidth={true}
                   icon={<Diversity1 />}
+                  onChange={e => setPassengersLimit(Number(e.target.value))}
                 />
               </>
             ) : (
@@ -120,16 +178,17 @@ export function BusContentModal({
                 <InfoTextField
                   label="Fornecedor"
                   defaultValue="Escon"
-                  disabled={userPermission()}
+                  disabled={true}
                   fullWidth={true}
                   icon={<Commute />}
                 />
                 <InfoTextField
                   icon={<Tag />}
                   fullWidth={true}
-                  disabled={userPermission()}
+                  disabled={permission}
                   label="Linha"
                   defaultValue={bus.line.toString()}
+                  onChange={e => setLine(Number(e.target.value))}
                 />
               </>
             )}
@@ -137,19 +196,52 @@ export function BusContentModal({
         </Grid>
 
         {mode !== 'view' && (
-          <Grid
-            container
-            sx={{ pt: 2 }}
-            spacing={10}
-            alignContent="center"
-            justifyContent="space-between"
-          >
-            <Grid item>
-              <Button variant="contained" color="info">
-                Editar
-              </Button>
+          <>
+            <Grid
+              container
+              sx={{ pt: 2 }}
+              spacing={10}
+              alignContent="center"
+              justifyContent="space-between"
+            >
+              <Grid item>
+                <Button
+                  onClick={() => setPermission(!permission)}
+                  variant="contained"
+                  color="info"
+                >
+                  Editar
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="error">
+                  Deletar
+                </Button>
+              </Grid>
+              {changes.length > 0 && (
+                <Grid item>
+                  <Button
+                    onClick={() => setChangesWarning(true)}
+                    variant="contained"
+                    color="success"
+                  >
+                    Salvar
+                  </Button>
+                </Grid>
+              )}
             </Grid>
-          </Grid>
+            {changesWarning && (
+              <>
+                <WarningField
+                  severity="warning"
+                  title="Tem certeza?"
+                  message={`Os seguintes campos foram alterados: ${changes.map(
+                    change => change
+                  )}`}
+                />
+              </>
+            )}
+          </>
         )}
       </DialogContent>
     </Dialog>
