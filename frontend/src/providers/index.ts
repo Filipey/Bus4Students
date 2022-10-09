7ebd7bbe11fe1c363d8c1a389e78753e8b088a74
Filipe <filipeasm18@gmail.com>
2022-10-08 22:24:45 -0300

    Chore: Add standard ContentType

--- frontend/src/providers/index.ts
+++ frontend/src/providers/index.ts
@@ -1,3 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
-export const Api = axios.create({ baseURL: 'http://localhost:8080/api/v1' });
+export const Api = axios.create({
+  baseURL: 'http://localhost:8080/api/v1',
+  headers: { 'Content-Type': 'application/json' }
+})
